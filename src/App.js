0d422a98a925b8923c4b565702b7aa9871abb66a
cybercube21 <cybercube@mailbox.org>
2022-01-31 12:46:45 +0100

    Changed Default React Stuff / Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import './styles.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav class="navbar">
+         <div class="navbar__container">
+            <div class="navbar__toggle" id="mobile-menu">
+               <span class="bar"></span>
+               <span class="bar"></span>
+               <span class="bar"></span>
+            </div>
+            <ul class="navbar__menu">
+               <li class="navbar__item">
+                  <a href="https://cybercube21.de" class="navbar__links"> Home </a>
+               </li>
+               <li class="navbar__item">
+                  <a href="/" onclick="memes_alert();" class="navbar__links"> Memes </a>
+               </li>
+               <li class="navbar__item">
+                  <a href="https://cybercube21.de/contact.html" class="navbar__links"> Contact </a>
+               </li>
+            </ul>
+         </div>
+      </nav>
     </div>
   );
 }

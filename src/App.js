28e44b522f517811fdeb824993fe00f4b694a8d8
Cybercube21 <cybercube@mailbox.org>
2022-02-10 22:36:27 +0100

    Added Video & img integration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,66 @@
 import './App.css';
 import './styles.css';
-import './modal.js';
-import './modal.css';
+
+
+var meme;
+var meme_json;
+var meme_fileExt;
+
+function httpGet(theUrl) {
+   var xmlHttp = new XMLHttpRequest();
+   xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+   xmlHttp.send( null );
+   return xmlHttp.responseText;
+}
+
+function reset_meme(){
+   // Make img hidden
+   var el = document.getElementById('hiddencontainer')
+   el.children[0].setAttribute('src', '')
+   el.setAttribute('style', '')
+   el.setAttribute('style', 'display: none;')
+
+   // Make video hidden
+   el.children[1].setAttribute('src', '')
+   el.setAttribute('style', '')
+   el.setAttribute('style', 'display: none;')
+}
+
+function load_meme(){
+   reset_meme()
+   meme = httpGet('https://api.cybercube21.de/memes')
+   meme_json = JSON.parse(meme)
+   meme_fileExt = meme_json.url.split('.').pop();
+
+   var el = document.getElementById('hiddencontainer')
+   var video = document.getElementsByTagName('video')
+   if (meme_fileExt === "mp4") {
+      // Make img hidden
+      el.children[0].setAttribute('src', '')
+
+      // Show Video
+      el.children[1].setAttribute('src', meme_json.url)
+      el.setAttribute('style', 'display: inline;')
+      el.children[1].controls = true;
+   }
+
+   else if (meme_fileExt === "webp") {
+      // Make Video hidden
+      el.children[1].setAttribute('src', '')
+
+      // Show img
+      el.children[0].setAttribute('src', meme_json.url)
+      el.setAttribute('style', 'display: inline;')
+   }
+   else {
+      reset_meme()
+   }
+ }
 
 function App() {
   return (
     <div className="App">
       <nav className="navbar">
-         <div id='navbar__title'>Cubie's delicious Memes</div>
          <div className="navbar__container">
             <div className="navbar__toggle">
                <span className="bar"></span>
@@ -30,12 +83,10 @@ function App() {
       <div className="center-object">
         <div className="wrapper">
           <main className='page-main'>
-            <button id='modal_button'>Test Button</button>
-            <div id="container__modal" className="modal">
-               <div className="container__pop-up" id="hiddencontainer">
-                  <span className="close" />
-                  <img alt="lul" className="container__pop-up-img" src="" id="hiddenimage" />
-               </div>
+            <button className='load_button' id='modal_button' onClick={load_meme}>Test Button</button>
+            <div className="container__pop-up" id="hiddencontainer">
+               <img alt="" className="container__pop-up-img" src="" id="hiddenimage"/>
+               <video alt="" className="container__pop-up-img" src="" id="hiddenimage" autoPlay playsInline loop/>
             </div>
           </main> 
         </div>

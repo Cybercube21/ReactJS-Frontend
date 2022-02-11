dba70ac98f17d8d2276889828d73689036c8ca6f
cybercube21 <cybercube@mailbox.org>
2022-02-11 11:13:36 +0100

    Changed Meme loading to React Object

--- src/App.js
+++ src/App.js
@@ -1,71 +1,10 @@
 import './App.css';
 import './styles.css';
 
-var meme;
-var meme_json;
-var meme_fileExt;
+import MemeContainer from './meme_container';
 
-function httpGet(theUrl) {
-   var xmlHttp = new XMLHttpRequest();
-   xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-   xmlHttp.send( null );
-   return xmlHttp.responseText;
-}
-
-function reset_meme(){
-   // Make img hidden
-   var el = document.getElementById('hiddencontainer')
-   el.children[0].setAttribute('src', '')
-   el.setAttribute('style', '')
-   el.setAttribute('style', 'display: none;')
-
-   // Make video hidden
-   el.children[1].setAttribute('src', '')
-   el.setAttribute('style', '')
-   el.setAttribute('style', 'display: none;')
-}
-
-function load_meme(){
-   reset_meme()
-   meme = httpGet('https://api.cybercube21.de/memes')
-   meme_json = JSON.parse(meme)
-   meme_fileExt = meme_json.url.split('.').pop();
-
-   var el = document.getElementById('hiddencontainer')
-   if (meme_fileExt === "webm") {
-      // Make img hidden
-      el.children[0].setAttribute('src', '')
-
-      // Show Video
-      el.children[1].setAttribute('src', meme_json.url)
-      el.setAttribute('style', 'display: inline;')
-      el.children[1].controls = true;
-   }
-
-   else if (meme_fileExt === "webp") {
-      // Make Video hidden
-      el.children[1].setAttribute('src', '')
-
-      // Show img
-      el.children[0].setAttribute('src', meme_json.url)
-      el.setAttribute('style', 'display: inline;')
-   }
-   else {
-      reset_meme()
-   }
- }
 
 function App() {
-
-   function load_memes(){ 
-    
-      var el = document.getElementById('hiddencontainer')
-            el.children[1].setAttribute('src', '')
-            el.setAttribute('style', '')
-    
-      el.setAttribute('style', 'display: inline;')
-    }
-
   return (
     <div className="App">
       <nav className="navbar">
@@ -92,22 +31,14 @@ function App() {
          </div>
       </nav>
       <div className="center-object">
-        <div className='load_button'>
-          <button id='modal_button' onClick={load_memes}>Test Button</button>
-        </div>
         <div className="wrapper">
           <main className='page-main'>
-             <p className='headline'>Welcome to Cubie's delicious memes</p> 
-            <button className='load_button' id='modal_button' onClick={load_meme}><p className='panel_text'>Give me a meme!</p></button>
-            <div className="container__pop-up" id="hiddencontainer">
-               <img alt="" className="container__pop-up-img" src="" id="hiddenimage"/>
-               <video alt="" className="container__pop-up-img" src="" id="hiddenimage" autoPlay playsInline loop/>
-            </div>
+            <MemeContainer />
           </main> 
         </div>
       </div>
       <div className='footer'>
-         <p className='p'>Wanna include them in your Website? Visit <a class="a" href="https://api.cybercube21.de">my API reference</a></p>
+         <p className='p'>Wanna include them in your Website? Visit <a className="a" href="https://api.cybercube21.de">my API reference</a></p>
       </div>
     </div>
   );
---
+++ src/meme_container.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class MemeContainer extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={ext:undefined};
+    }
+
+    async fetch_meme(url){
+        var response = await fetch(url);
+        var meme_json = await response.json();
+        return meme_json;
+     }
+
+    async load_meme() {
+
+        var meme_json = await this.fetch_meme('https://api.cybercube21.de/memes'); 
+        var meme_fileExt = meme_json.url.split('.').pop();
+
+        this.setState({ext:meme_fileExt,url:meme_json.url});
+    }
+
+    render() {
+
+        const state = this.state;
+        var element= <div></div>;
+
+        if(state.ext === "webp") {
+            element = <img alt="" className="container__pop-up-img" src={state.url} id="hiddenimage"/>
+        } else if (state.ext === "webm") {
+            element = <video alt="" className="container__pop-up-img" src={state.url} id="hiddenimage" controls autoPlay playsInline loop/>
+        }
+
+        return (
+            <div>
+                <p className='headline'>Welcome to Cubie's delicious memes</p> 
+                <button className='load_button' id='modal_button' onClick={this.load_meme.bind(this)}><p className='panel_text'>Give me a meme!</p></button>
+                <div className="container__pop-up" id="hiddencontainer">
+                    {element}
+                </div>
+            </div>
+        )
+
+    }
+
+}
+
+export default MemeContainer;

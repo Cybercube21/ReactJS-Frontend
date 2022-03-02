d23d2a4c8878d09f1cca9400d690be29d1ef9b42
cybercube21 <cybercube@mailbox.org>
2022-03-02 07:44:11 +0100

    Added button to reload the last meme

--- src/meme_container.js
+++ src/meme_container.js
@@ -5,7 +5,7 @@ class MemeContainer extends React.Component {
         super(props);
         this.state={ext:undefined};
         this.state={share_link:undefined}
-        this.state={last_share_link:undefined}
+        this.state={back_link:undefined}
     }
 
     async fetch_meme(url){
@@ -14,17 +14,21 @@ class MemeContainer extends React.Component {
         return meme_json;
      }
 
-    async load_meme() {
-
-        if (meme_fileName !== undefined){
-            var last_share_link = meme_fileName;
-        }
+    async load_meme(share_link) {
+        if (share_link !== undefined){
+            this.setState({back_link:"https://api.cybercube21.de/memes/" + share_link})
+        } 
 
         var meme_json = await this.fetch_meme('https://api.cybercube21.de/memes'); 
         var meme_fileExt = meme_json.url.split('.').pop();
         var meme_fileName = meme_json.url.split('/').pop();
 
         this.setState({ext:meme_fileExt,url:meme_json.url,share_link:meme_fileName});
+    }
+
+    async reload_meme(back_link){
+        var meme_fileExt = back_link.split('.').pop();
+        this.setState({ext:meme_fileExt,url:back_link,share_link:back_link,back_link:undefined});
     }
 
     render() {
@@ -36,7 +40,9 @@ class MemeContainer extends React.Component {
 
         if(state.ext === "webp") {
             element = <img alt="" className="container__pop-up-img" src={state.url} id="hiddenimage"/>
-        } else if (state.ext === "webm") {
+        } 
+        
+        else if (state.ext === "webm") {
             element = <video alt="" className="container__pop-up-img" src={state.url} id="hiddenimage" controls playsInline />
         }
 
@@ -44,15 +50,15 @@ class MemeContainer extends React.Component {
             share_button = <button className='load_button' id="share_button" onClick={() => {navigator.clipboard.writeText("https://api.cybercube21.de/share/" + this.state.share_link)}}><p className='panel_text'>Share this meme!</p><p>(copy link to clipboard)</p></button>
         }
 
-        if (this.state.last_share_link !== undefined) {
-            share_button = <button className='load_button' id="back_button" onClick={() => {element = <video alt="" className="container__pop-up-img" src={last_share_link} id="hiddenimage" controls playsInline />}}><p className='panel_text'>Share this meme!</p><p>(copy link to clipboard)</p></button>
+        if (this.state.back_link !== undefined) {
+            back_button = <button className='load_button' id="back_button" onClick={this.reload_meme.bind(this, state.back_link)}><p className='panel_text'>Reload the last meme!</p></button>
         }
 
         return (
             <div>
                 <div>
                     <p className='headline'>Welcome to Cubie's meme collection!</p> 
-                    <button className='load_button' id='modal_button' onClick={this.load_meme.bind(this)}><p className='panel_text'>Give me a meme!</p></button>
+                    <button className='load_button' id='modal_button' onClick={this.load_meme.bind(this, state.share_link)}><p className='panel_text'>Give me a meme!</p></button>
                     {share_button}
                     {back_button}
                 </div>
